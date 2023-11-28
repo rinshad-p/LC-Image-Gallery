31b2b96d3e2a1ed99f4100f5220546fa47bfb5ee
Rinshad <learn.rinshad@gmail.com>
2023-11-28 21:52:11 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const images = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png'];
+const alts = {
+  'img1.png': 'Photo of Sundar Pichai',
+  'img2.png': 'Photo of Steve Jobs',
+  'img3.png': 'Photo of Elon Musk',
+  'img4.png': 'Photo of Bill Gates',
+  'img5.png': 'Photo of Mark Zuckerberg',
+};
+
+/* Looping through images */
+
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('alt', alts[image]);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', (e) => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
